c66d70ed9d3ca67a50743b0e9bde95cf1b83213b
Sullivan Dovie <drdovie@blacksburgeye.com>
2023-09-25 16:02:38 -0400

    Configure LLM for Langchain/OpenAI and Ensure Correct Example Output

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import openai
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.llms.openai import OpenAI
 from dotenv import load_dotenv
 from os import getenv
 
@@ -8,60 +10,68 @@ class LLMFactory:
     """
     def __init__(self, api_key):
         self.api_key = api_key
-        openai.api_key = self.api_key
 
     def create_llm_instance(self):
-        return LLM()
+        return LLM(self.api_key)
 
 
 class LLM:
     """
     Main Class - created with the instance of the Factory Class
     """
-
-    def __init__(self):
+    def __init__(self, api_key):
+        self.api_key = api_key
         self.schema = None
 
+    def _initialize_chain(self):
+        model = OpenAI(openai_api_key = self.api_key, temperature=0)
+        primer = """
+            You are a helpful assistant that translates natural language to SQL Queries. 
+            You only respond with the correct SQL Query and nothing else, NEVER including anything but exactly the SQL Query desired.
+            You have been working with the database for a long time, so you know it very well: {schema}\n
+        """.format(schema=self.schema)
+        prompt = PromptTemplate(
+            input_variables=["question"], 
+            template = primer + "{question}"
+        )
+        self.chain = LLMChain(llm=model, prompt=prompt)
+
     def load_schema_as_string(self, schema_str):
-        """
-        Load Schema as String
-        """
         self.schema = schema_str
+        self._initialize_chain()
 
     def load_schema_from_file(self, schema_file):
-        """
-        Load Schema from File
-        """
-        self.schema = "" # placeholder
+        self.schema = ""
+        self._initialize_chain()
 
-    def convert_text_to_sql(self, prompt):
-        """
-        Convert Text to SQL
-        """
-        # https://platform.openai.com/docs/guides/gpt openai documentation
-        model = "gpt-3.5-turbo-0613"
-        messages = [
-            {"role": "system", "content": ""}, # Sets behavior
-            {"role": "user", "content": prompt}, # User question/prompt
-            {"role": "assistant", "content": ""} # Conversation history, schema, useful data
-        ]
-        response = openai.ChatCompletion.create(model=model, messages=messages)
-        sql_query = response["choices"][0]["message"]["content"]
-        return sql_query
+    def convert_text_to_sql(self, text):
+        if not self.chain:
+            return "Chain must be initialized before query."
+        sql = self.chain.run(question=text).strip()
+        return sql
 
     def visualize_data(self, dataframe):
-        """
-        Visualize Data
-        """
-        print(dataframe) # placeholder
+        print(dataframe)
 
-    def refine_query(self, prompt):
-        """
-        Refine Query
-        """
-        refined_query = prompt # placeholder
+    def refine_query(self, text):
+        refined_query = text
         return refined_query
 
+
+schema = """
+CREATE TABLE Books (
+    BookID INT AUTO_INCREMENT PRIMARY KEY,
+    Title VARCHAR(100) NOT NULL,
+    AuthorID INT,
+    PublisherID INT,
+    PublishedYear YEAR,
+    ISBN VARCHAR(20),
+    PageCount INT,
+    Price DECIMAL(8,2),
+    FOREIGN KEY (AuthorID) REFERENCES Authors(AuthorID),
+    FOREIGN KEY (PublisherID) REFERENCES Publishers(PublisherID)
+);
+"""
 
 # Example usage
 if __name__ == '__main__':
@@ -70,8 +80,9 @@ if __name__ == '__main__':
 
     factory = LLMFactory(api_key=api_key)
     llm_instance = factory.create_llm_instance()
-    llm_instance.load_schema_from_file('schema.sql')
+    llm_instance.load_schema_as_string(schema)
 
-    prompt = input()
-    sql = llm_instance.convert_text_to_sql(prompt)
+    # text = input()
+    text = "Find the total price of books for each author, where the total price is greater than 100, for books published after the year 2000 and have a PageCount greater than 300. List the results by AuthorID. The result should include the AuthorID and the total price of the books."
+    sql = llm_instance.convert_text_to_sql(text)
     print(sql)
