fbeda7ab0ffac4e0df7bda00470bfda0c3611e7a
Elhanan <w0w_proo@yahoo.com>
2023-09-29 23:28:06 -0600

    Database connector function

--- db_connector.py
+++ db_connector.py
@@ -1,4 +1,10 @@
 # Connect to the hosted DB and return the connection
+import os
+import psycopg2
+from dotenv import load_dotenv
+load_dotenv()
 
 def get_connection():
-    pass
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    connection = psycopg2.connect(DATABASE_URL)
+    return connection
