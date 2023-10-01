1aa7d50d29f4d18a86e8d3135080f573caa01413
Sullivan Dovie <drdovie@blacksburgeye.com>
2023-10-01 15:40:17 -0400

    Converted from SQL Queries to API Calls

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 from os import getenv
 from model import load_model
+import json
 
 class LLMFactory:
     """
@@ -23,50 +25,64 @@ class LLM:
         self.model_type = model_type
         self.model_name = model_name
         self.api_key = api_key
-        self.schema = None
+        self.api_structure = None
 
     def _initialize_chain(self):
         # Model
         model = load_model(self.model_type, self.model_name, self.api_key, temperature=0)
 
         template = """
-            You are an experienced data analyst with specialized knowledge in SQL databases and queries. 
-            You can provide accurate SQL queries based on the provided database schema and make use of your expert knowledge of SQL and databases.
-            The purpose is to assist with formulating SQL queries based on the information available, maintaining the accuracy and relevance of the responses.
-            For valid and clear questions related to the provided database, provide the appropriate SQL query as a response and nothing else.
-            If a question is asked that is incomplete, ambiguous, unclear, or unrelated to the provided database, respond with \"I did not understand.'\" and explain why you don't understand.
-            Database Schema:
-            {schema}
-            Any numbers including {{numbers}} are valid.
-            {{question}}
-        """.replace('  ', '').format(schema=self.schema).strip()
+            You are an advanced API interpreter. Your task is to extract the HTTP method, endpoint, and body of the API request from the provided command.
+            Given the API structure and a detailed command, parse the command to identify these elements of the API request. Respond with each element in a structured format.
+            If the command is not valid or unclear, respond with "INVALID QUESTION".
+
+            For example, for a command "Update the name and status of pet 112 to 'Buddy' and 'sold' respectively.", the response is:
+            "method": "POST",
+            "endpoint": "/pet/112",
+            "body": (
+                "name": "Buddy",
+                "status": "sold"
+            )
+
+            If the body should be empty, leave it as ().
+            Do not respond with anything besides the method, endpoint, and body.
+
+            API Structure:
+            {api_structure}
+            Command: {{command}}
+        """.replace('  ', '').format(api_structure=api_structure.replace('{', '(').replace('}', ')')).strip()
+
         prompt = PromptTemplate(
-            input_variables=["question", "numbers"], 
+            input_variables=["command"], 
             template = template
         )
+
         self.chain = LLMChain(llm=model, prompt=prompt)
 
-
-    def load_schema_as_string(self, schema_str: str):
-        self.schema = schema_str.strip()
+    def load_api_structure_as_string(self, api_structure_str: str):
+        self.api_structure = api_structure_str.strip()
         self._initialize_chain()
 
 
-    def load_schema_from_file(self, schema_file: str):
-        with open(schema_file, 'r') as file:
-            self.schema = file.read().strip()
-        self._initialize_chain()
+    def load_api_structure_from_file(self, api_structure_file: str):
+        with open(api_structure_file, 'r') as file:
+            self.api_structure = file.read().strip()
+        self._initialize_chain()  
 
-    def convert_text_to_sql(self, text: str):
+    def convert_text_to_command(self, text: str):
         if not self.chain:
             return "Chain must be initialized before query."
         if len(text) == 0:
             return "I did not understand. Can you please provide more details?"
         if text[-1] != '.' and text[-1] != '?':
-            text += "?"
-        numbers = ', '.join((word[:-1] if word[:-1].isdigit() else word) for word in text.split() if word.isdigit() or word[:-1].isdigit())
-        sql = self.chain.run(question=text, numbers=numbers).strip()
-        return sql
+            text += "."
+
+        response = self.chain.run(command=text)
+        if "INVALID QUESTION" in response:
+            return "I did not understand. Can you please provide more details?"
+        response = response.replace('(', '{').replace(')', '}')
+        dict = json.loads('{' + response + '}')
+        return str(dict)
 
     def visualize_data(self, dataframe):
         print(dataframe)
@@ -75,33 +91,24 @@ class LLM:
         refined_query = text
         return refined_query
 
-
-schema = """
-CREATE TABLE Books (
-    BookID INT AUTO_INCREMENT PRIMARY KEY,
-    Title VARCHAR(100) NOT NULL,
-    AuthorID INT,   
-    PublisherID INT,
-    PublishedYear YEAR,
-    ISBN VARCHAR(20),
-    PageCount INT,
-    Price DECIMAL(8,2),
-    FOREIGN KEY (AuthorID) REFERENCES Authors(AuthorID),
-    FOREIGN KEY (PublisherID) REFERENCES Publishers(PublisherID)
-);
-"""
-
 # Example usage
 if __name__ == '__main__':
     load_dotenv()
     api_key = getenv('OPENAI_API_KEY')
 
+    api_structure = ""
+    with open("api_structure.txt", "r") as file:
+        api_structure = file.read().strip()
+
     factory = LLMFactory(api_key)
 
     while True:
-        model_type = input("Model Type: ").lower().strip()
-        model_name = input("Model Name: ").lower().strip()
+        # model_type = input("Model Type: ").lower().strip()
+        # model_name = input("Model Name: ").lower().strip()
+        model_type = "chatopenai"
+        model_name = "gpt-4-0613"
         llm_instance = factory.create_llm_instance(model_type=model_type, model_name=model_name)
-        llm_instance.load_schema_as_string(schema)
+        llm_instance.load_api_structure_as_string(api_structure)
         text = input("Prompt: ")
-        print(llm_instance.convert_text_to_sql(text) + '\n')
+        # text = "Update the status of pet 789 to 'available'."
+        print(llm_instance.convert_text_to_command(text) + '\n')
