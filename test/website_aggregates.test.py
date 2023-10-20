3387e7eb294b6529d8449afd3c6613849cd024fd
Vignesh Prakash <pranomvignesh@gmail.com>
2023-10-20 10:56:01 -0700

    Update website_aggregates.test.py

--- test/website_aggregates.test.py
+++ test/website_aggregates.test.py
@@ -9,4 +9,19 @@ This testcases should follow a format
     sql_output : '...' -> SQL output,
     description : '...' -> Any Description (Optional)
 }]
+
+
+Natural Language Inputs:
+-- How many total visitors have visited this domain?
+-- How many total visitors have visited this domain in the last 7 days?
+-- How many total visitors have visited this domain in the year 2022? 
+-- How many total visitors have visited this domain whose employee count is within <a given range > ? 
+-- How many total visitors have visited this domain whose revenue range is within <a given range > ? 
+-- How many total visitors have visited this domain from the country <insert country here> ? 
+-- How many total visitors have visited this domain whose revenue range is within <a given range >, whose employee count range is within <a given range> and whose last_visit_date is <date> ? 
+-- How many total visitors have visited this domain whose employee count is within < this count range > ? 
+-- How many total visitors have visited this domain whose industry is <insert industry> ? 
+-- How many <insert some measure> have visited this domain whose <some other condition> ? 
+​
+Note: This is not an exhaustive list of all the queries, if you feel there might be some more type of queries, add those too.
 """
