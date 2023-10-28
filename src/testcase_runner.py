5e27d8e5ab229372bd9d1eb7245f4b3c1b05d8cd
Elhanan <w0w_proo@yahoo.com>
2023-10-28 17:33:48 -0600

    Upload other files

---
+++ src/testcase_runner.py
@@ -0,0 +1,10 @@
+"""
+This function has to be used to run the test cases.
+
+This should output benchmarking results ( Like accuracy )
+
+This module should expose functions to
+1. Run the test cases
+
+I leave the structure of this module implementation to the developer.
+"""
---
+++ test/website_aggregates.test.py
@@ -0,0 +1,27 @@
+"""
+Here the testcases for the function has to be added
+
+This testcases should follow a format
+
+[{
+    input : '...' -> Input from user,
+    output : '...' -> Output from the model, 
+    sql_output : '...' -> SQL output,
+    description : '...' -> Any Description (Optional)
+}]
+
+
+Natural Language Inputs:
+-- How many total visitors have visited this domain?
+-- How many total visitors have visited this domain in the last 7 days?
+-- How many total visitors have visited this domain in the year 2022? 
+-- How many total visitors have visited this domain whose employee count is within <a given range > ? 
+-- How many total visitors have visited this domain whose revenue range is within <a given range > ? 
+-- How many total visitors have visited this domain from the country <insert country here> ? 
+-- How many total visitors have visited this domain whose revenue range is within <a given range >, whose employee count range is within <a given range> and whose last_visit_date is <date> ? 
+-- How many total visitors have visited this domain whose employee count is within < this count range > ? 
+-- How many total visitors have visited this domain whose industry is <insert industry> ? 
+-- How many <insert some measure> have visited this domain whose <some other condition> ? 
+​
+Note: This is not an exhaustive list of all the queries, if you feel there might be some more type of queries, add those too.
+"""
