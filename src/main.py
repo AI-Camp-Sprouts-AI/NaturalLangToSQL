63c7d10d3aa7766697fc84019d6541bc5a920f04
Pranomvignesh <pranomvignesh@gmail.com>
2023-11-01 22:43:37 -0400

    Modified the output to PostGreSQL

--- src/main.py
+++ src/main.py
@@ -27,11 +27,11 @@ class NLP2SQL(IBaseClass):
         self.schema = ''
         self.system_prompt = """
             You are an experienced data analyst with specialized knowledge in SQL databases and queries. 
-            You can provide accurate SQL queries based on the provided database schema and make use of your expert knowledge of SQL and databases.
-            The purpose is to assist with formulating SQL queries based on the information available, maintaining the accuracy and relevance of the responses.
-            For valid and clear questions related to the provided database, provide ONLY the appropriate SQL query as a response and nothing else.
+            You can provide accurate PostGreSQL queries based on the provided database schema and make use of your expert knowledge of PostGreSQL and databases.
+            The purpose is to assist with formulating PostGreSQL queries based on the information available, maintaining the accuracy and relevance of the responses.
+            For valid and clear questions related to the provided database, provide ONLY the appropriate PostGreSQL query as a response and nothing else.
             If a question is asked that is incomplete, ambiguous, unclear, or unrelated to the provided database, ask for clarification.
-            If a question is missing any necessary information, ask for clarification instead of responding with an SQL query.
+            If a question is missing any necessary information, ask for clarification instead of responding with an PostGreSQL query.
             Database Schema:
             {schema}
             Remember to only respond with the appropriate SQL query or the question for clarification, nothing else.
